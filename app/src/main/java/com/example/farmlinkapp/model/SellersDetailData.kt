fbe82eeb7e87efc6e5350d5d9f86bfda25188860
Aishwary Dixit <mr.dixit17@gmail.com>
2024-09-01 15:11:57 +0530

    Designed the UI of SellerDetails Page and created a new data class (SellersDetailData)

---
+++ app/src/main/java/com/example/farmlinkapp/model/SellersDetailData.kt
@@ -0,0 +1,11 @@
+package com.example.farmlinkapp.model
+
+data class SellersDetailData(val name: String,
+                        val vegetable: String,
+                        val distance: Double,
+                        val quantity: Int,
+                        val pricePerKg: Double,
+                        val rating: Double,
+                        val reviewCount:Int
+) {
+}
